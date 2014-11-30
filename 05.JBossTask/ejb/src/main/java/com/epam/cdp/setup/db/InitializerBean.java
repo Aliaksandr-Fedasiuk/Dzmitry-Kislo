ac5dce760bd321fa322d8aff829c0c9cb4d182c8
Dzmitry Kislo <Dzmitry_Kislo@epam.com>
2014-11-30 18:44:40 +0300

    Add liquibase for db-migration

---
+++ 05.JBossTask/ejb/src/main/java/com/epam/cdp/setup/db/InitializerBean.java
@@ -0,0 +1,53 @@
+package com.epam.cdp.setup.db;
+
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.DatabaseException;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import liquibase.resource.ResourceAccessor;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Created by dima on 30.11.14.
+ */
+@Startup
+@Singleton
+@TransactionManagement(TransactionManagementType.BEAN)
+public class InitializerBean {
+    private static final String STAGE = "development";
+    private static final String CHANGELOG_FILE = "liquibase.xml";
+
+    @Resource
+    private DataSource dataSource;
+
+    @PostConstruct
+    protected void bootstrap() {
+        ResourceAccessor resourceAccessor = new ClassLoaderResourceAccessor(getClass().getClassLoader());
+        try {
+            Connection connection = dataSource.getConnection();
+            JdbcConnection jdbcConnection = new JdbcConnection(connection);
+            Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(jdbcConnection);
+
+            Liquibase liquibase = new Liquibase(CHANGELOG_FILE, resourceAccessor, database);
+            liquibase.update(STAGE);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (DatabaseException e) {
+            e.printStackTrace();
+        } catch (LiquibaseException e) {
+            e.printStackTrace();
+        }
+    }
+}
